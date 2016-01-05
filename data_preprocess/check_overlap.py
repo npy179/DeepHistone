7c9280cb3cf383fc07508bd9dc26c3cea9cffc53
npy179 <npy179@gmail.com>
2016-01-05 16:11:37 -0500

    modify check_overlap.py

--- data_preprocess/check_overlap.py
+++ data_preprocess/check_overlap.py
@@ -2,9 +2,15 @@ import os
 import sys
 import glob
 
+folder = sys.argv[1]
+os.chdir(folder)
+
 bedfiles = glob.glob("*.bed")
+
+
 for i in range(0,len(bedfiles)):
     for j in range(0, len(bedfiles)):
         new_overlape_name = bedfiles[i].replace("E017-","").replace(".bed","")+"_"+bedfiles[j].replace("E017-","").replace(".bed","")
-        exc = "bedtools intersect "+"-a "+bedfiles[i]+" -b "+bedfiles[j]+" > "+ new_overlape_name
-        os.system(exc)
+        #exc = "bedtools intersect "+"-a "+bedfiles[i]+" -b "+bedfiles[j]+" > "+ new_overlape_name
+        #os.system(exc)
+        print new_overlape_name
