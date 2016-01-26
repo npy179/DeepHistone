48694068f1a09a2c5e1a53e264cc08781882a0cf
npy179 <npy179@gmail.com>
2016-01-26 14:51:49 -0500

    modify cat_matrix.py

--- data_preprocess/cat_matrix.py
+++ data_preprocess/cat_matrix.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python
 import numpy as np
+import sys
 
-seq_label_back = np.load("seq_label_background.npy")
 
-seq_fore = np.load("H3K27me3.sort.merge.delete_600.bed.matrix.npy")
-row, col = seq_fore.shape
-label_fore = np.ones((row,1),dtype=seq_fore.dtype)
-seq_label_fore = np.c_[seq_fore, label_fore]
+seq_label_back = np.load("background_label_seq.npy")
+seq_label_fore_name = sys.argv[1]
+
+seq_label_fore = np.load(seq_label_fore_name)
+
+row, col = seq_label_fore.shape
 
 seq_label_fore_back = np.r_[seq_label_back, seq_label_fore]
 
 np.random.shuffle(seq_label_fore_back)
 
-np.save("seq_label_fore_back",seq_label_fore_back)
-
 seq_fore_back = seq_label_fore_back[:,:-1]
 label_fore_back_float16 = seq_label_fore_back[:,-1]
 label_fore_back_int64 = label_fore_back_float16.astype("int64")
 
-np.save("seq_fore_back",seq_fore_back)
-np.save("label_fore_back",label_fore_back_int64)
+
+whole_sequence_name = seq_label_fore_name.split("_")[0]+"_sequence_fore_back"
+whole_label_name = seq_label_fore_name.split("_")[0]+"_label_fore_back"
+np.save(whole_sequence_name, seq_fore_back)
+np.save(whole_label_name, label_fore_back_int64)
+
+#print whole_sequence_name
+#print whole_label_name
